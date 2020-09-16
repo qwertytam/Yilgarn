e739396b4664931b84d2d8803249a551e882072d
Tom Marshall <tom.q.marshall@gmail.com>
2020-09-16 19:28:03 -0400

    Added sympy to required packages

--- usdi-xau.py
+++ usdi-xau.py
@@ -31,7 +31,7 @@ import sys
 import subprocess
 import pkg_resources
 
-required = {'fecon236', 'pandas', 'numpy', 'sklearn', 'statsmodels'}
+required = {'fecon236', 'pandas', 'numpy', 'sklearn', 'statsmodels', 'sympy'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
